283b767d55a083d8fb6dca74ed136c314628c6cd
Humberto Machado <betolinck@gmail.com>
2016-10-09 14:34:13 -0300

    using primitive type

--- src/JsonLoader.ts
+++ src/JsonLoader.ts
@@ -2,7 +2,7 @@ import * as fs from "fs";
 import * as path from "path";
 
 export class JsonLoader {
-    public static loadFile<T>(filePath: String, fileName: String): T | null {
+    public static loadFile<T>(filePath: string, fileName: string): T | null {
         let file: string = path.join(filePath, fileName);
         if (fs.existsSync(file)) {
             return <T>JSON.parse(fs.readFileSync(file, { encoding: 'utf8' }).toString());
